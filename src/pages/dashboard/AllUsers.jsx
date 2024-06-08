78e721a9a668ed8b3ebbb0e0ce5dc8b4395996af
rkshawn <rkshawn975@gmail.com>
2024-06-08 10:43:51 +0600

    Make admin functionality added.Now an admin can change the role of a regular user to admin!!

--- src/pages/dashboard/AllUsers.jsx
+++ src/pages/dashboard/AllUsers.jsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../hooks/useAxiosSecure";
 import Swal from "sweetalert2";
+import { HiMiniUserGroup } from "react-icons/hi2";
 
 const AllUsers = () => {
   const axiosSecure = useAxiosSecure();
@@ -53,6 +54,39 @@ const AllUsers = () => {
     });
    
    
+  }
+
+  //make an existing user admin
+  const handleMakeAdmin=(user)=>{
+    console.log(user.name)
+    Swal.fire({
+      title: "Are you sure?",
+      text: `You want to make admin ${user.name}`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.patch(`/users/admin/${user._id}`)
+        .then(res=>{
+          console.log(res.data)
+          if(res.data.modifiedCount>0){
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: `${user.name} is an admin now!!!`,
+              showConfirmButton: false,
+              timer: 1500
+            });
+            refetch();
+          }
+        })
+
+       
+      }
+    });
   }
  
   return (
@@ -112,7 +146,20 @@ const AllUsers = () => {
                   </dialog>
                 </td>
                 <td>
-                  <button  className="btn btn-xs">Make Admin</button>
+                  {
+                    user.role ==='admin'?
+                    <>
+                    Admin
+                    </>
+                    :
+                    <>
+                    <button
+                    onClick={()=>handleMakeAdmin(user)}
+                    className="btn btn-xs">
+                      <HiMiniUserGroup className="text-xl" ></HiMiniUserGroup>
+                    </button>
+                    </>
+                  }
                 </td>
                 <td>
                   <button onClick={()=>handleBlockUser(user)} className={`${user.status==='blocked'? 'disabled':''} btn btn-xs`}>{user?.status}</button>
